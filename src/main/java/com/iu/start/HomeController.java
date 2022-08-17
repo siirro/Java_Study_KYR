0c5944d34c6afea4706a0aae32a68f216623bd2c
kdy991101 <kdy991101@naver.com>
2022-08-17 16:09:10 +0900

    1

--- src/main/java/com/iu/start/HomeController.java
+++ src/main/java/com/iu/start/HomeController.java
@@ -4,10 +4,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +17,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	@Autowired
-	SqlSession sqlSession;
-	
-	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
@@ -30,8 +24,6 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		
-		//System.out.println(sqlSession);
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
