c089676b70bbc4a4420d83586864d1c07df58b46
goodeecomputer <viostarryy@gmail.com>
2022-08-30 15:40:54 +0900

    220830

--- src/main/webapp/resources/js/dom_3_ex1.js
+++ src/main/webapp/resources/js/dom_3_ex1.js
@@ -1,16 +1,33 @@
-
 const btn = document.getElementById('btn');
 const btn2 = document.getElementById('btn2');
 const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
 const d1 = document.getElementById('d1')
 const d2 = document.getElementById('d2')
 const d3 = document.getElementById('d3')
+const select = document.getElementById('select');
+const c1 = document.getElementsByClassName('c1');
 //익명함수, 이벤트리스너
 
 btn.addEventListener("click", function(){
-
-    d3.value = d1.value*1+d2.value*1;
-    
+    console.log("operater : "+select.value);
+    //d3.value = d1.value*1+d2.value*1;
+    if(select.value=='+') {
+        console.log('더하기')
+        d3.value = d1.value*1+d2.value*1;
+    } else if(select.value=='-') {
+        console.log('빼기')
+        d3.value = d1.value*1-d2.value*1;
+    } else if(select.value=='*') {
+        console.log('곱하기')
+        d3.value = (d1.value*1)*(d2.value*1);
+    } else if(select.value=='/') {
+        console.log('나누기')
+        d3.value = (d1.value*1)/(d2.value*1);
+    } else if(select.value=='%') {
+        console.log('나머지')
+        d3.value = (d1.value*1)%(d2.value*1);
+    }
 });
 
 btn2.addEventListener("click", function(){
@@ -19,4 +36,15 @@ btn2.addEventListener("click", function(){
 
 btn3.addEventListener("click", function(){
     btn2.click();
+})
+
+btn4.addEventListener("click", function(){
+    alert(c1);
+    console.log(c1);
+    
+    for(let i=0;i<c1.length;i++){
+        console.log(c1[i].selected);
+    }
+
+    c1[1].selected=true;
 })
