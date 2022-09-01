55f3ffe3ed9c0bfd229ab086dd600f686428e11c
goodeecomputer <viostarryy@gmail.com>
2022-09-01 10:33:29 +0900

    .

--- src/main/webapp/resources/js/dom_6.js
+++ src/main/webapp/resources/js/dom_6.js
@@ -1,9 +1,15 @@
 const btn1 = document.getElementById('btn1');
 const d1 = document.getElementById('d1');
+const d2 = document.getElementById('d2');
+const d3 = document.getElementById('d3');
 const list = document.getElementById('list');
 const list2 = document.getElementById('list2');
 const add = document.getElementById('add');
 const add2 = document.getElementById('add2');
+const add3 = document.getElementById('add3');
+const add4 = document.getElementById('add4');
+const add5 = document.getElementById('add5');
+
 
 btn1.addEventListener("click",function(){
     d1.innerHTML = '<img src="/resources/images/title1.jpg">'
@@ -23,8 +29,30 @@ add2.addEventListener("click",function(){
     let h = document.createElement('h3');
     let t = document.createTextNode("hello");
 
-    h.append(t);
-    li.append(h);
+    h.appendChild(t);
+    li.appendChild(h);
     list2.prepend(li);
 
+})
+
+add3.addEventListener("click",function(){
+    let i = document.createElement("input");
+    let ch = document.createAttribute("type");
+    ch.value="checkbox";
+    i.setAttributeNode(ch);
+    d2.appendChild(i);
+})
+
+add4.addEventListener("click",function(){
+    let h = document.createElement("h3");
+    let t = document.createTextNode("Before");
+    h.appendChild(t);
+    d3.before(h);
+})
+
+add5.addEventListener("click",function(){
+    let h = document.createElement("h3");
+    let t = document.createTextNode("After");
+    h.appendChild(t);
+    d3.after(h);
 })
